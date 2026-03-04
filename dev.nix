b9969aad700e8a036a035e717059190d83af548b
Nathan Gasser <hello@gougoule.ch>
2026-03-04 21:19:43 +0100

    add dev.nix for development env

---
+++ dev.nix
@@ -0,0 +1,26 @@
+{ pkgs ? import <nixpkgs> {
+    config = {
+        allowUnfree = true;
+        cudaSupport = true;
+    };
+} }:
+
+let
+    nvidiaPackage = pkgs.linuxPackages.nvidiaPackages.stable;
+in
+    pkgs.mkShell {
+        packages = with pkgs; [
+            gcc
+
+            cudatoolkit
+            cudaPackages.cuda_cudart
+            nvidiaPackage
+        ];
+
+        shellHook = ''
+    export CUDA_PATH=${pkgs.cudatoolkit}
+    export CUDAToolkit_ROOT=${pkgs.cudatoolkit}
+    export LD_LIBRARY_PATH="${nvidiaPackage}/lib:$LD_LIBRARY_PATH"
+        '';
+
+    }
