2e0a8a31cf0e21bcd7dbb5cbd28225cfc83bfb2f
Nathan Gasser <hello@gougoule.ch>
2026-03-08 17:05:20 +0100

    basic allocations for creating CNN

--- main.cu
+++ main.cu
@@ -11,15 +11,14 @@
 #define LAYER_TYPE_MLP 2
 
 typedef struct Convolution_layer {
-    int input_dimensions;
+    int output_dimensions;
     int filter_dimensions;
-    int filter_stride;
     DATA_TYPE* filter_parameters;
     DATA_TYPE filter_bias;
 } Convolution_layer;
 
 typedef struct Pooling_layer {
-    int input_dimensions;
+    int output_dimensions;
     int pool_dimensions;
     int pool_type; 
 } Pooling_layer;
@@ -41,7 +40,7 @@ typedef struct Layer {
         Convolution_layer convolution_layer;
         Pooling_layer pooling_layer;
         MLP_layer mlp_layer;
-    } layer;
+    };
 } Layer;
 
 typedef struct CNN {
@@ -56,17 +55,33 @@ int create_cnn(CNN* cnn, int input_dimensions, int num_layers, Layer layers[]) {
         Layer layer = layers[i];
 
         if(layer.layer_type == LAYER_TYPE_CONVOLUTION) {
-            // TODO: calculate output dimensions
-            cudaMalloc(&(layer.layer.convolution_layer.filter_parameters), layer.layer.convolution_layer.filter_dimensions * layer.layer.convolution_layer.filter_dimensions * sizeof(DATA_TYPE));
+            cudaMalloc(&(layer.convolution_layer.filter_parameters), layer.convolution_layer.filter_dimensions * layer.convolution_layer.filter_dimensions * sizeof(DATA_TYPE));
         } else if(layer.layer_type == LAYER_TYPE_POOLING) {
             // Nothing to allocate for pooling layer
-            // TODO: calculate output dimensions
         } else if(layer.layer_type == LAYER_TYPE_MLP) {
-            // TODO: finish this after the two TODOs above are done
-            cudaMalloc(&(layer.layer.mlp_layer.neurons), layer.layer.mlp_layer.num_neurons * sizeof(Neuron));
+            cudaMalloc(&(layer.mlp_layer.neurons), layer.mlp_layer.num_neurons * sizeof(Neuron));
             cudaDeviceSynchronize();
-            for(int j = 0; j < layer.layer.mlp_layer.num_neurons; j++) {
+
+            int num_input = 0;
+            if(i == 0) {
+                num_input = input_dimensions * input_dimensions; // Assuming input is square
+            } else {
+                Layer prev_layer = layers[i - 1];
+                if(prev_layer.layer_type == LAYER_TYPE_CONVOLUTION) {
+                    num_input = prev_layer.convolution_layer.output_dimensions * prev_layer.convolution_layer.output_dimensions;
+                } else if(prev_layer.layer_type == LAYER_TYPE_POOLING) {
+                    num_input = prev_layer.pooling_layer.output_dimensions * prev_layer.pooling_layer.output_dimensions;
+                } else if(prev_layer.layer_type == LAYER_TYPE_MLP) {
+                    num_input = prev_layer.mlp_layer.num_neurons;
+                }
+            }
+
+            for(int j = 0; j < layer.mlp_layer.num_neurons; j++) {
                 Neuron neuron;
+                cudaMalloc(&(neuron.weights), num_input * sizeof(DATA_TYPE));
+                cudaDeviceSynchronize();
+                neuron.num_weights = num_input;
+                cudaMemcpy(&(layer.mlp_layer.neurons[j]), &neuron, sizeof(Neuron), cudaMemcpyHostToDevice);
             }
 
         }
@@ -85,51 +100,60 @@ int main() {
     Layer layers[] = {
         {
             .layer_type = LAYER_TYPE_CONVOLUTION,
-            .layer.convolution_layer = {
-                .filter_dimensions = 3, // 28x28 -> 26x26
-                .filter_stride = 1,
+            .convolution_layer = {
+                .output_dimensions = 26,
+                .filter_dimensions = 3 // 28x28 -> 26x26
             }
         },
         {
             .layer_type = LAYER_TYPE_CONVOLUTION,
-            .layer.convolution_layer = {
+            .convolution_layer = {
+                .output_dimensions = 24,
                 .filter_dimensions = 3, // 26x26 -> 24x24
-                .filter_stride = 1,
             }
         },
         {
             .layer_type = LAYER_TYPE_POOLING,
-            .layer.pooling_layer = {
+            .pooling_layer = {
+                .output_dimensions = 12,
                 .pool_dimensions = 2, // 24x24 -> 12x12
                 .pool_type = POOL_TYPE_MAX
             }
         },
         {
             .layer_type = LAYER_TYPE_POOLING,
-            .layer.pooling_layer = {
+            .pooling_layer = {
+                .output_dimensions = 6,
                 .pool_dimensions = 2, // 12x12 -> 6x6
                 .pool_type = POOL_TYPE_MAX
             }
         },
         {
             .layer_type = LAYER_TYPE_MLP,
-            .layer.mlp_layer = {
+            .mlp_layer = {
                 .num_neurons = 30
             }
         },
         {
             .layer_type = LAYER_TYPE_MLP,
-            .layer.mlp_layer = {
+            .mlp_layer = {
                 .num_neurons = 30
             }
         },
         {
             .layer_type = LAYER_TYPE_MLP,
-            .layer.mlp_layer = {
+            .mlp_layer = {
                 .num_neurons = 10
             }
         }
     };
+
+    create_cnn(&cnn, 28, 7, layers);
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        fprintf(stderr, "CUDA error after %s at %s:%d: %s\n", "hi", __FILE__, __LINE__, cudaGetErrorString(err)); \
+    }
+
 
     return 0;
 }
