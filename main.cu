4f2eee4b4b802085f3119f2f4c83d17d60a55afd
Nathan Gasser <hello@gougoule.ch>
2026-03-22 17:13:31 +0100

    yeah...

--- main.cu
+++ main.cu
@@ -13,7 +13,7 @@
 #define NUM_CYCLES 100
 #define DATASET_SIZE 60000
 #define TEST_DATASET_SIZE 10000
-#define LEARNING_RATE 1e-4
+#define LEARNING_RATE 5e-5
 
 int main() {
     printf("Hello, CUDA!\n");
@@ -26,11 +26,11 @@ int main() {
 
     Layer* layers = (Layer*)malloc(sizeof(*layers) * 6);
 
-    create_convolution_layer(&(layers[0]), 28, 26, 3, 6, 1, 6);
-    create_pooling_layer(&(layers[1]), 26, 13, 2, 6);
-    create_convolution_layer(&(layers[2]), 13, 10, 4, 12, 6, 12);
-    create_pooling_layer(&(layers[3]), 10, 5, 2, 12);
-    create_mlp_layer(&(layers[4]), 5*5*12, 128);
+    create_convolution_layer(&(layers[0]), 28, 26, 3, 16, 1, 16);
+    create_pooling_layer(&(layers[1]), 26, 13, 2, 16);
+    create_convolution_layer(&(layers[2]), 13, 10, 4, 32, 16, 32);
+    create_pooling_layer(&(layers[3]), 10, 5, 2, 32);
+    create_mlp_layer(&(layers[4]), 5*5*32, 128);
     create_mlp_layer(&(layers[5]), 128, 10);
 
     NN nn = {
@@ -44,11 +44,14 @@ int main() {
     for(int cycle = 0; cycle < NUM_CYCLES; cycle++) {
         printf("Cycle %d\n", cycle);
 
-        call_nn(&nn, test_dataset[0].pixels);
         display_nn_output_mnist(&nn, test_dataset[0].label);
+
         int correct_predictions = 0;
         for(int i = 0; i < TEST_DATASET_SIZE; i++) {
             call_nn(&nn, test_dataset[i].pixels);
+            if(i % 1000 == 0) {
+                display_nn_output_mnist(&nn, test_dataset[i].label);
+            }
             DATA_TYPE output[10];
             cudaMemcpy(output, nn.layers[nn.num_layers - 1].output.d1.output, 10 * sizeof(DATA_TYPE), cudaMemcpyDeviceToHost);
 
@@ -79,6 +82,8 @@ int main() {
         fprintf(accuracy_file, "cycle %d: %.2f%%\n", cycle, (float)correct_predictions / TEST_DATASET_SIZE * 100.0f);
         fclose(accuracy_file);
 
+        DATA_TYPE learning_rate = LEARNING_RATE * (1.0f - (float)cycle / NUM_CYCLES);
+
         for(int i = 0; i < (DATASET_SIZE - BATCH_SIZE); i += BATCH_SIZE) {
             zero_grads_nn(&nn);
             for(int j = 0; j < BATCH_SIZE; j++) {
@@ -88,7 +93,7 @@ int main() {
                 call_nn(&nn, dataset[i + j].pixels);
                 grad_nn(&nn, dataset[i + j].label);
             }
-            update_nn(&nn, LEARNING_RATE);
+            update_nn(&nn, learning_rate);
         }
         
     }
